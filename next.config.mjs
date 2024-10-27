3a0825d0410b41fc982cdc156bba3f070865279f
IRahman2024 <stormfuryultra784@gmail.com>
2024-10-27 22:08:53 +0600

    4th attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    },
+    webpack(config, { isServer }) {
+      // Ensure compatibility for server and client-side builds
+      if (!isServer) {
+        config.resolve.fallback = { fs: false, path: false };
+      }
+  
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      };
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
